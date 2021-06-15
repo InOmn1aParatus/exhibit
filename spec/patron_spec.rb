4f427cfe9acd00e87c6a619ce5d2337d7cb5dc6f
Matt Kragen <mattdkragen@gmail.com>
2021-06-15 15:43:34 -0700

    Add README and finish iteration 1

---
+++ spec/patron_spec.rb
@@ -0,0 +1,30 @@
+require './lib/patron'
+
+RSpec.describe Patron do
+  context 'instantiation' do
+    it 'exists' do
+      patron_1 = Patron.new("Bob", 20)
+      expect(patron_1).to be_a(Patron)
+    end
+
+    it 'has attributes' do
+      patron_1 = Patron.new("Bob", 20)
+      expect(patron_1.name).to eq('Bob')
+      expect(patron_1.spending_money).to eq(20)
+    end
+
+    it 'start with no interests' do
+      patron_1 = Patron.new("Bob", 20)
+      expect(patron_1.interests).to eq([])
+    end
+  end
+
+  context 'methods' do
+    it 'adds interests' do
+      patron_1 = Patron.new("Bob", 20)
+      patron_1.add_interest('Dead Sea Scrolls')
+      patron_1.add_interest('Gems and Minerals')
+      expect(patron_1.interests).to eq(['Dead Sea Scrolls', 'Gems and Minerals'])
+    end
+  end
+end
